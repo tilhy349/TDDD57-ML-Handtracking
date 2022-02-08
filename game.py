2f3db03effb484de3374c1140de24a04fd720d57
Tilda Hylander <hylandertilda@gmail.com>
2022-02-08 17:19:32 +0100

    Change color of background if successful placing

--- game.py
+++ game.py
@@ -18,10 +18,13 @@ class Game:
 
         self.hands = Hand()
 
-        self.box1 = Box(100, 100, 50, 100, pygame.Color(220, 20, 20))
-        self.box2 = Box(250, 100, 60, 40, pygame.Color(220, 20, 180))
-        self.box3 = Box(500, 100, 100, 100, pygame.Color(20, 20, 220))
-
+        self.boxes = [Box(100, 100, 50, 100, pygame.Color(220, 20, 20)), 
+                      Box(250, 100, 60, 40, pygame.Color(220, 20, 180)), 
+                      Box(500, 100, 100, 100, pygame.Color(20, 20, 220))]
+        
+        self.areas = [pygame.Rect(300, 400, 150, 150), 
+                      pygame.Rect(500, 400, 70, 50), 
+                      pygame.Rect(600, 400, 80, 120)]
 
     def load_camera(self):
         #Store the current frame from webcam
@@ -34,36 +37,55 @@ class Game:
         # Initialing Color
         self.surface.fill((0,0,0))
 
+        self.check_accuracy()
+        
+        for i in range(len(self.boxes)):
+            self.boxes[i].update_pos_selected(self.hands)
+            self.boxes[i].draw(self.surface)
+
         #Change position of boxes if they are selected
-        self.box1.update_pos_selected(self.hands)
-        self.box2.update_pos_selected(self.hands)
-        self.box3.update_pos_selected(self.hands)
+        # self.box1.update_pos_selected(self.hands)
+        # self.box2.update_pos_selected(self.hands)
+        # self.box3.update_pos_selected(self.hands)
 
         # Drawing Rectangle, Rect(left, top, width, height)
-        self.box1.draw(self.surface)
-        self.box2.draw(self.surface)
-        self.box3.draw(self.surface)
+        # self.box1.draw(self.surface)
+        # self.box2.draw(self.surface)
+        # self.box3.draw(self.surface)
         
-        #Draw rectangles to inser the boxes in
-        pygame.draw.rect(self.surface, (66, 150, 178), pygame.Rect(300, 400, 150, 150), 1)
-        pygame.draw.rect(self.surface, (66, 150, 178), pygame.Rect(500, 400, 70, 50), 1)
-        pygame.draw.rect(self.surface, (66, 150, 178), pygame.Rect(600, 400, 80, 120), 1)
+        #Draw rectangles to insert the boxes in
+        pygame.draw.rect(self.surface, (66, 150, 178), self.areas[0], 1)
+        pygame.draw.rect(self.surface, (66, 150, 178), self.areas[1], 1)
+        pygame.draw.rect(self.surface, (66, 150, 178), self.areas[2], 1)
     
     
     #Controll the boxes is being selectede or not  
     def check_the_boxes(self):
-        self.box1.collide(self.hands)
-        self.box2.collide(self.hands)
-        self.box3.collide(self.hands)   
-      
-        
+        self.boxes[0].collide(self.hands)
+        self.boxes[1].collide(self.hands)
+        self.boxes[2].collide(self.hands)   
+
+    def check_accuracy(self):
+        nr_of_boxes_true = 0
+
+        for i in self.boxes:
+            for j in self.areas:
+                if j.contains(i):
+                    nr_of_boxes_true += 1
+
+        if nr_of_boxes_true == len(self.boxes):
+            self.surface.fill((0, 100, 0))
+        else:
+            self.surface.fill((0, 0, 0))
+            
+   
     def update(self):
         self.load_camera()
 
         #Draw landmarks and process hand positions
         self.frame = self.hands.process_hands(self.frame)
 
-        # --- LAB 1: hold over to select & pinch to select --
+        # --- LAB 1: hold over to select & pinch to select ---
         #Compare hand position to boxes
         #Change color saturation if marker is covering one of the boxes
         self.check_the_boxes()
@@ -72,6 +94,9 @@ class Game:
         
         #Draw hand marker based on hand position
         self.hands.draw_marker(self.surface)
+
+        #Function which checks the accuracy of all boxes
+        #check_accuracy()
         
         #Show webcam with landmarks on screen
         cv2.imshow("Frame", cv2.flip(self.frame, 1))
